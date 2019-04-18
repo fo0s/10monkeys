47356cecf89907b244e9e093f02f0d5c86bb7e82
fo0s <fo0sies@gmail.com>
2019-04-18 10:31:59 +0100

    Updated to working site validator. Created space for flow chart diagram

--- lib/monkey_crawler.rb
+++ lib/monkey_crawler.rb
@@ -1,20 +1,40 @@
-require 'open-uri'
-require 'nokogiri'
-require 'json'
 require "net/http"
 
-def randomize_site()
+def monkey_crawler
+    safety = 1
+    sites = []
+    working = 0
+
+    while safety < 5
+        site = randomize_site
+        if isAlive?(site)
+            puts site
+            working += 1
+        else 
+            puts "It works!!!!!"
+        end
+        sites << site
+        safety += 1
+    end
+end
+
+def randomize_site
     min_length = rand(1..5)
     max_length = rand(10..30)
 
     ('a'..'z').to_a.shuffle[min_length,max_length].join
 end
 
-def isAlive?(site)
-    url = URI.parse("www." + site + ".com")
-    req = Net::HTTP.new(url.host, url.port)
-    res = req.request_head(url.path)
-    puts res.code
+def isAlive?(url_str)
+  # create workable url
+  site1 = "https://www." + url_str + ".com"
+  site2 = "https://" + url_str + ".com"
+  begin
+    Net::HTTP.get_response(URI.parse(site1)).is_a?(Net::HTTPSuccess)
+    Net::HTTP.get_response(URI.parse(site2)).is_a?(Net::HTTPSuccess)
+  rescue
+    false
+  end
 end
 
-puts checkAlive
+monkey_crawler
