553d15c0b180ff5a41c551fa4f5275034be39507
fo0s <fo0sies@gmail.com>
2019-04-18 12:18:04 +0100

    Refactored monkey_crawler.rb

--- lib/monkey_crawler.rb
+++ lib/monkey_crawler.rb
@@ -1,40 +1,44 @@
 require "net/http"
 
-def monkey_crawler
-    safety = 1
-    sites = []
-    working = 0
+# Crawl my beauties!!
+class MonkeyCrawler
+  attr_accessor :run_monkey
+  attr_reader :monkeys, :site_hits
 
-    while safety < 5
-        site = randomize_site
-        if isAlive?(site)
-            puts site
-            working += 1
-        else 
-            puts "It works!!!!!"
-        end
-        sites << site
-        safety += 1
+  def initialize
+    @run_monkey = true
+    @monkeys = []
+    @site_hits = 0
+    monkey_crawler
+  end
+
+  def monkey_crawler
+    while @run_monkey == true
+      site = randomize_site
+
+      if isAlive?(site)
+        @monkeys << site
+        @site_hits += 1
+      end
     end
-end
+  end
 
-def randomize_site
+  def randomize_site
     min_length = rand(1..5)
     max_length = rand(10..30)
 
     ('a'..'z').to_a.shuffle[min_length,max_length].join
-end
+  end
 
-def isAlive?(url_str)
-  # create workable url
-  site1 = "https://www." + url_str + ".com"
-  site2 = "https://" + url_str + ".com"
-  begin
-    Net::HTTP.get_response(URI.parse(site1)).is_a?(Net::HTTPSuccess)
-    Net::HTTP.get_response(URI.parse(site2)).is_a?(Net::HTTPSuccess)
-  rescue
-    false
+  def isAlive?(url_str)
+    # create workable url
+    site1 = "https://www.#{url_str}.com/"
+    site2 = "https://#{url_str}.com/"
+    begin
+      Net::HTTP.get_response(URI.parse(site1)).is_a?(Net::HTTPSuccess)
+      Net::HTTP.get_response(URI.parse(site2)).is_a?(Net::HTTPSuccess)
+    rescue
+      false
+    end
   end
 end
-
-monkey_crawler
