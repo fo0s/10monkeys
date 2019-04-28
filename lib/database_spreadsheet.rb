098859a9f29ea09fa96f50d1013a7fa142f75052
fo0s <fo0sies@gmail.com>
2019-04-28 22:13:41 +0100

    Connecting classes

--- lib/database_spreadsheet.rb
+++ lib/database_spreadsheet.rb
@@ -6,7 +6,7 @@ class GoogleDatabase
     # Authenticate a session with your Service Account
     session = GoogleDrive::Session.from_service_account_key('./../database/client_secret.json')
     # Get the spreadsheet by its title
-    spreadsheet = @session.spreadsheet_by_title('Monkey Database')
+    spreadsheet = session.spreadsheet_by_title('Monkey Database')
     # Get the first worksheet
     @worksheet = spreadsheet.worksheets.first
   end
@@ -16,21 +16,21 @@ class GoogleDatabase
     @worksheet.rows.each { |row| puts row.first(3).join(',') }
   end
 
-  def write_database
+  def write_database(input)
     # inster into specific row
-    worksheet.insert_rows(2, [['Hello!', 'This', 'was', 'inserted', 'via', 'Ruby']])
+    @worksheet.insert_rows(2, input)
     save_worksheet
   end
 
   def update_database
     # Update targeted cell. This could also be "A2"
-    worksheet[2, 1] = 'Updated!'
+    @worksheet[2, 1] = 'Updated!'
     save_worksheet
   end
 
   def delete_database
     # Deletes a row
-    worksheet.delete_rows(2, 1)
+    @worksheet.delete_rows(2, 1)
     save_worksheet
   end
 
@@ -45,7 +45,7 @@ class GoogleDatabase
   end
 
   def save_worksheet
-    worksheet.save
+    @worksheet.save
     puts 'Database saved'
   end
 end
