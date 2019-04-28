dfe8cb677f74603aeb873a20e8280628e1ddcb74
fo0s <fo0sies@gmail.com>
2019-04-28 21:08:07 +0100

    Expanded on tests

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem "nokogiri"
+gem "google_drive"
+gem "httparty"
+gem "byebug"
+gem "simplecov"
---
+++ lib/monkey_scraper.rb
@@ -0,0 +1,37 @@
+require 'byebug'
+require 'nokogiri'
+require 'open-uri'
+require 'pp'
+
+class MonkeyScrapper
+  attr_reader :result
+
+  def monkey_scraper(site)
+
+    html = Nokogiri::HTML(open site)
+
+    # Shortlist only words that contain letters
+    text  = html.at('body').inner_text.scan(/[a-z]+/i)
+
+    # Sanity checks
+    # -------------
+
+    # byebug
+    # pp text.length, text.uniq.length, text.uniq.sort[0..10]
+
+    # -------------
+
+    # Produce list
+    @result = frequencies(text)
+  end
+
+  # Find and sort the most frequent words
+  def frequencies(words)
+    Hash[
+      words.group_by(&:downcase).map{ |word,instances|
+        [word,instances.length]
+      }.sort_by(&:last).reverse
+    ]
+  end
+
+end
