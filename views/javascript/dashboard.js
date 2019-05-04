0483ac392cca6f30b35f8abbe99a8774c40b6153
fo0s <fo0sies@gmail.com>
2019-05-04 20:05:45 +0100

    Simple frontend styling

---
+++ views/javascript/dashboard.js
@@ -0,0 +1,24 @@
+// const menuIcon = $('menu-icon');
+// const sidenav = $('.sidenav');
+// const sidenavClose = $('.sidenav__close-icon');
+
+// // Add and remove provided class names
+// function toggleClassName(el, className) {
+//   if (el.hasClass(className)) {
+//     el.removeClass(className);
+//   } else {
+//     el.addClass(className);
+//   }
+// }
+
+
+
+// // Open the side nav on click
+// menuIcon.on('click', function() {
+//   toggleClassName(sidenav, 'active');
+// });
+
+// // Close the side nav on click
+// sidenavClose.on('click', function() {
+//   toggleClassName(sidenav, 'active');
+// });
